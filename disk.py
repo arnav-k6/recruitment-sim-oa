27b3523fe69196e3db6f29f9395bd80faf09bd3a
Arnav <120742713+arnav-k6@users.noreply.github.com>
2025-08-29 22:55:57 -0300

    Update disk.py

--- disk.py
+++ disk.py
@@ -11,5 +11,15 @@ def final_disk_speed(height: float, length: float, incline: float, mass: float, 
     :param radius: the radius of the disk (meters)
     :return: the speed of the disk (m/s)
     """
+    # Formula used: GPE1 + KE1 + RotationalKE1 = GPE2 + KE1 + RotationalKE2
+    # mgh + 0 + 0 = 0 + 1/2*mv^2 + 1/2*I*w^2
+    # mgh = 1/2*mv^2 + 1/2*I*w^2
+    # mgh = 1/2*mv^2 + 1/4*m*r^2*(v^2/r^2)
+    # mgh = 1/2*mv^2 + 1/4*m*v^2
+    # mgh = 3/4*mv^2
+    # gh = 3/4*v^2
+    # v = sqrt(4/3*gh)
+    # Note: I am assuming no slipping (only rolling through the incline), so friction is neglected.
+    return math.sqrt((4/3)*9.81*height)
 
 
