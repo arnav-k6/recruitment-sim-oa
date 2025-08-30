3df2a560db5ebc9dd979a9fbfa4a11cd23fb65f9
Arnav <120742713+arnav-k6@users.noreply.github.com>
2025-08-29 22:53:10 -0300

    Update disk.py

--- disk.py
+++ disk.py
@@ -1,10 +1,9 @@
 import numpy as np
-
+import math
 
 def final_disk_speed(height: float, length: float, incline: float, mass: float, friction: float, radius: float) -> float:
     """
     Returns the speed of a uniform disk after it reaches the bottom of an inclined slope.
-
     :param height: the height of the incline (meters)
     :param length: the length of the slope (meters)
     :param incline: the angle of the slope (degrees)
@@ -13,3 +12,14 @@ def final_disk_speed(height: float, length: float, incline: float, mass: float, 
     :param radius: the radius of the disk (meters)
     :return: the speed of the disk (m/s)
     """
+    # Formula used: GPE1 + KE1 + RotationalKE1 = GPE2 + KE1 + RotationalKE2
+    # mgh + 0 + 0 = 0 + 1/2*mv^2 + 1/2*I*w^2
+    # mgh = 1/2*mv^2 + 1/2*I*w^2
+    # mgh = 1/2*mv^2 + 1/4*m*r^2*(v^2/r^2)
+    # mgh = 1/2*mv^2 + 1/4*m*v^2
+    # mgh = 3/4*mv^2
+    # gh = 3/4*v^2
+    # v = sqrt(4/3*gh)
+    # Note: I am assuming no slipping (only rolling through the incline), so friction is neglected.
+    return math.sqrt((4/3)*9.81*height)
+
